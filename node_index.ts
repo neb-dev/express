a8fe014be5874662980259ed58b18eb1c14edb25
neb <jneb.dev@outlook.com>
2025-04-29 19:43:37 -0600

    removes node index files, adds tsconfig and js file

--- node_index.ts
+++
@@ -1,8 +0,0 @@
-import http from 'http'
-import { nodeIndexRoutes } from './node_index_routes'
-
-const app = http.createServer(nodeIndexRoutes)
-
-app.listen(3000, () => {
-  console.log('Node.js server is running on port 3000')
-})
