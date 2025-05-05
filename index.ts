51e68d0630602893408b257ea37c4208172d5501
neb <jneb.dev@outlook.com>
2025-05-05 14:26:00 -0600

    adds cors

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import attendeeRouter from './routes/attendee.routes'
+import cors from 'cors'
 
 dotenv.config()
 export const app = express()
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(attendeeRouter)
